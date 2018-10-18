b381d499e1c73b802f153e814fc070ae96e443b1
Dawid <dawidweltrowski@gmail.com>
2018-10-18 16:26:09 +0200

    Test2

--- CinemaApi/Controllers/MoviesController.cs
+++ CinemaApi/Controllers/MoviesController.cs
@@ -20,7 +20,7 @@ namespace CinemaApi.Controllers
         [Route("GetMovies")]
         public ActionResult GetMovies()
         {
-            var movielol = "x";
+            
             using (var context = new CinemaDbContext())
             {
                 var movie = context.Movies.Where(m => m.Id == 1).FirstOrDefault();
