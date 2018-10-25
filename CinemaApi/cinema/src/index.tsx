19c85959f7d031bddda8d3c030bd64b532f21474
Dawid Weltrowski <dawidw-k@wp.pl>
2018-10-25 16:43:17 +0200

    Dodany został plik ScrollToTop.tsx dzięki któremu przy zmianie route strona będzie na górze

--- CinemaApi/cinema/src/index.tsx
+++ CinemaApi/cinema/src/index.tsx
@@ -4,10 +4,13 @@ import './assets/css/index.css'
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 
 ReactDOM.render(
 <BrowserRouter>
+<ScrollToTop>
 <App />
+</ScrollToTop>
 </BrowserRouter>,
   
   document.getElementById('root') as HTMLElement
