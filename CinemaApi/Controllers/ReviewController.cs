80731f72d2b028ebbbc56328eb64715efb6c39b8
Dawid Weltrowski-Knopik <dawidw-k@wp.pl>
2018-12-18 03:40:53 +0100

    Dodalem Review

---
+++ CinemaApi/Controllers/ReviewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApi.Controllers
+{
+    [Route("cinema/")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly CinemaDBContext context;
+        public ReviewController(CinemaDBContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        [Route("GetReviews")]
+        public ActionResult GetReviews(int id)
+        {
+            var reviewList = context.Review.Where(a => a.IdMovies == id).ToList();
+            if (reviewList.Count != 0)
+                return Ok(reviewList);
+
+            return NotFound();
+        }
+
+        [Route("AddReview")]
+        [HttpPost]
+        public ActionResult AddReview(Review review)
+        {
+            context.Review.Add(new Review
+            {
+                Author = review.Author,
+                Review1 = review.Review1,
+                IdMovies = review.IdMovies
+            });
+
+            context.SaveChanges();
+            return Ok(review);
+        }
+    }
+}
